243959bd9c04dbc46a5cc5bead725b11a547678e
evan slack <evan.slack@outlook.com>
2026-06-13 17:34:06 -0400

    consumable

---
+++ core/src/consumable.rs
@@ -0,0 +1,43 @@
+use crate::planet::Planets;
+#[cfg(feature = "python")]
+use pyo3::pyclass;
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "python", pyclass(eq))]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum Consumable {
+    Planet(Planets),
+    // Tarot(Tarot),         // future
+    // Spectral(Spectral),   // future
+}
+
+impl Consumable {
+    pub fn cost(&self) -> usize {
+        match self {
+            Self::Planet(p) => p.cost(),
+        }
+    }
+
+    pub fn name(&self) -> String {
+        match self {
+            Self::Planet(p) => p.name(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_consumable_planet_cost() {
+        let c = Consumable::Planet(Planets::Mercury);
+        assert_eq!(c.cost(), 3);
+    }
+
+    #[test]
+    fn test_consumable_planet_name() {
+        let c = Consumable::Planet(Planets::Mercury);
+        assert_eq!(c.name(), "Mercury");
+    }
+}
