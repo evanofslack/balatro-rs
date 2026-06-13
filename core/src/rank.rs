a3a3f0c9e0e4608fd0342c268ff6c4e6baa2904f
evan slack <evan.slack@outlook.com>
2026-06-13 17:35:19 -0400

    mv rank back
    mv

--- core/src/rank.rs
+++ core/src/rank.rs
@@ -1,2 +1,27 @@
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Hash, Copy)]
+pub struct Level {
+    pub level: usize,
+    pub chips: usize,
+    pub mult: usize,
+    pub plays: usize,
+}
 
-
+/// All the different possible hand ranks.
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Hash, Copy)]
+pub enum HandRank {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush,
+    FiveOfAKind,
+    FlushHouse,
+    FlushFive,
+}
